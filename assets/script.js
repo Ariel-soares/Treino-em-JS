d6a85a641be14b175df07bb1fe1a114c4e75c0d4
Ariel Soares <ariel.sfranco@gmail.com>
2025-01-23 20:37:55 -0300

    First commit for saving content

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+
+const axios = require('axios')
+
+axios.get(url).then(response => {
+    const funcionarios = response.data
+
+    const mulher = m => 
+
+    const procurada = funcionarios.filter()
+})
