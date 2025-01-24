ea8e8862cd2a7752753b7a68d32de4a40b512f12
Ariel Soares <ariel.sfranco@gmail.com>
2025-01-23 21:05:25 -0300

    exercise yet to be finished

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,19 @@ const axios = require('axios')
 axios.get(url).then(response => {
     const funcionarios = response.data
 
-    const mulher = m => 
+    const mulher = m => m.genero == 'M'
 
-    const procurada = funcionarios.filter()
+    const chines = c => c.pais == 'China'
+
+    const procurada = funcionarios.filter(mulher).filter(chines)
+
+    //console.log(procurada)
+
+    for(let i = 0; i <= procurada.lenght; i++){
+
+        console.log(procurada[i])
+        
+
+    }
+
 })
